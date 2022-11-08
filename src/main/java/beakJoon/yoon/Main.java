c599f0232c0425102b30e20e36dfab6aa2d4adc3
Fouink <yyss9103@gmail.com>
2022-11-08 18:21:55 +0900

    연결 요소의 개수 DFS

--- src/main/java/beakJoon/yoon/Main.java
+++ src/main/java/beakJoon/yoon/Main.java
@@ -3,64 +3,53 @@ package beakJoon.yoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int X, Y, K;
-	static boolean[][] visit;
-	static int[][] arr;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
+	static ArrayList<Integer>[] arr;
+	static boolean[] visit;
 	static int answer;
-	public static void main(String[] args) throws NumberFormatException, IOException {
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		int N = Integer.parseInt(st.nextToken());
 		int tc = Integer.parseInt(st.nextToken());
 
+		arr = new ArrayList[N + 1];
+		visit = new boolean[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			arr[i] = new ArrayList<>();
+			visit[i] = false;
+		}
+
 		for (int i = 0; i < tc; i++) {
-			answer = 0;
 			st = new StringTokenizer(br.readLine());
-			X = Integer.parseInt(st.nextToken());
-			Y = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
-			arr = new int[Y][X];
-			visit = new boolean[Y][X];
-			K = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < K; j++) {
-				st = new StringTokenizer(br.readLine());
-
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-
-				arr[y][x] = 1;
-			}
-
-			for (int j = 0; j < Y; j++) {
-				for (int k = 0; k < X; k++) {
-					if (arr[j][k] == 1 && !visit[j][k]) {
-						dfs(j, k);
-						answer++;
-					}
-				}
-			}
-			System.out.println(answer);
+			arr[a].add(b);
+			arr[b].add(a);
 		}
-
+		for (int i = 1; i <= N; i++) {
+			if (!visit[i]) {
+				dfs(i);
+				answer++;
+			}
+		}
+		System.out.println(answer);
 	}
 
-	static void dfs(int y, int x) {
-		visit[y][x] = true;
-		for (int i = 0; i < 4; i++) {
-			int ry = y + dy[i];
-			int rx = x + dx[i];
-			if (ry >= 0 && rx >= 0 && ry < Y && rx < X) {
-				if (arr[ry][rx] == 1 && !visit[ry][rx]) {
-					dfs(ry, rx);
-				}
+	public static void dfs(int start) {
+		visit[start] = true;
+		for (int i = 0; i < arr[start].size(); i++) {
+			int next = arr[start].get(i);
+			if (!visit[next]) {
+				dfs(next);
 			}
 		}
 	}
-
-
 }
