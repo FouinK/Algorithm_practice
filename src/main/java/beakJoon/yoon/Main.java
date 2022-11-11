65970e83662753bb2a550fa72f9850cab5d27747
Fouink <yyss9103@gmail.com>
2022-11-11 13:22:38 +0900

    케빈 베이컨의 6단계 법칙 (BFS 연결리스트), 1389번

--- src/main/java/beakJoon/yoon/Main.java
+++ src/main/java/beakJoon/yoon/Main.java
@@ -3,41 +3,76 @@ package beakJoon.yoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int N, R, C, answer;
+	static ArrayList<Integer>[] arr;
+	static boolean[] know;
+	static int cnt;
+	static int kevinCnt[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		partition(R, C, (int) Math.pow(2, N));
+		arr = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			arr[i] = new ArrayList<>();
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			arr[a].add(b);
+			arr[b].add(a);
+		}
+
+		int min = Integer.MAX_VALUE;
+		int answer = 0;
+
+		for (int i = 1; i <= N; i++) {
+			int sum = 0;
+			for (int j = 1; j <= N; j++) {
+				know = new boolean[N + 1];
+				kevinCnt = new int[N + 1];
+				if (i != j) {
+					sum += bfs(i, j);
+				}
+			}
+			if (sum < min) {
+				min = sum;
+				answer = i;
+			}
+		}
 
 		System.out.println(answer);
+
 	}
 
-	public static void partition(int r, int c, int size) {
-		if (size == 1) {
-			return;
+	public static int bfs(int start, int end) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(start);
+		know[start] = true;
+		while (!queue.isEmpty()) {
+			int start_point = queue.poll();
+			Iterator<Integer> iterator = arr[start_point].iterator();
+			while (iterator.hasNext()) {
+				int next = iterator.next();
+				if (!know[next]) {
+					kevinCnt[next] = kevinCnt[start_point] + 1;
+					if (next == end) {
+						return kevinCnt[next];
+					}
+				}
+				queue.offer(next);
+			}
 		}
-		int halfSize = size / 2;
-		if (r < halfSize && c < halfSize) {
-			partition(r,c,halfSize);
-		} else if (r < halfSize && c >= halfSize) {
-			answer += (size * size) / 4;
-			partition(r, c - halfSize, halfSize);
-		} else if (r >= halfSize && c < halfSize) {
-			answer += ((size * size) / 4) * 2;
-			partition(r - halfSize, c, halfSize);
-		} else {
-			answer += ((size * size) / 4) * 3;
-			partition(r - halfSize, c - halfSize, halfSize);
-		}
+		return 0;
 	}
 
 
