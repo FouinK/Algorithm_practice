c4e5ea60c5cf488aef96d5ee49501f5edd424a06
Fouink <yyss9103@gmail.com>
2022-11-13 13:56:55 +0900

    연결리스트 DFS BFS

--- src/main/java/beakJoon/yoon/Main.java
+++ src/main/java/beakJoon/yoon/Main.java
@@ -1,61 +1,78 @@
 package beakJoon.yoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr;
-	static int N;
-	static int M;
-	static int[] location1 = {-1, 1, 0, 0};
-	static int[] location2 = {0, 0, -1, 1};
-	static boolean[][] visit;
-	static int answer = 1;
+	static ArrayList<Integer>[] arr;
+	static boolean visit[];
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
 
-		N = sc.nextInt();
-		M = sc.nextInt();
-
-		arr = new int[N][M];
-		visit = new boolean[N][M];
-		visit[0][0] = true;
-
-		for (int i = 0; i < N; i++) {
-			String getInput = sc.next();
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = Integer.parseInt(String.valueOf(getInput.charAt(j)));
-			}
+		arr = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			arr[i] = new ArrayList<>();
 		}
 
-		bfs(0, 0);
-		System.out.println(arr[N-1][M-1]);
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			arr[a].add(b);
+			arr[b].add(a);
+		}
+
+		for (int i = 1; i <= N; i++) {
+			arr[i].sort((a, b) -> a - b);
+		}
+
+		visit = new boolean[N + 1];
+		dfs(start);
+		sb.append("\n");
+		visit = new boolean[N + 1];
+		bfs(start);
+
+		System.out.println(sb);
+
 	}
 
-	public static void bfs(int row, int col) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] {row, col});
+	public static void dfs(int start) {
+		sb.append(start).append(" ");
+		visit[start] = true;
+		for (int i = 0; i < arr[start].size(); i++) {
+			int next = arr[start].get(i);
+			if (!visit[next]) {
+				dfs(next);
+			}
+		}
+	}
 
+	public static void bfs(int start) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(start);
+		visit[start] = true;
 		while (!queue.isEmpty()) {
-			int[] now = queue.poll();
-			int nowRow = now[0];
-			int nowCol = now[1];
-			for (int i = 0; i < 4; i++) {
-				int nextRow = nowRow - location1[i];
-				int nextCol = nowCol - location2[i];
-
-				if (nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= M) {
-					continue;
+			int now = queue.poll();
+			sb.append(now).append(" ");
+			for (int i = 0; i < arr[now].size(); i++) {
+				int next = arr[now].get(i);
+				if (!visit[next]) {
+					visit[next] = true;
+					queue.offer(next);
 				}
-
-				if (arr[nextRow][nextCol] == 0 || visit[nextRow][nextCol]) {
-					continue;
-				}
-				queue.offer(new int[]{nextRow, nextCol});
-				visit[nextRow][nextCol] = true;
-				arr[nextRow][nextCol] = arr[nowRow][nowCol]+1;
 			}
 		}
 	}
