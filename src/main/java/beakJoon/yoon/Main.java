556ea4fdf0f41589a781f8c0ca6ffbd8610c791b
Fouink <yyss9103@gmail.com>
2022-11-25 15:40:35 +0900

    백준 9019 DSLR

--- src/main/java/beakJoon/yoon/Main.java
+++ src/main/java/beakJoon/yoon/Main.java
@@ -3,62 +3,87 @@ package beakJoon.yoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[] cnt = new int[101];
-	static int[] ladderAndSnake = new int[101];
-	static boolean[] visit = new boolean[101];
 
+	static boolean[] visit;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int ladderSize = Integer.parseInt(st.nextToken());
-		int snakeSize = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine());
 
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < ladderSize+snakeSize; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			int answer = Integer.parseInt(st.nextToken());
 
-			ladderAndSnake[a] = b;
+			visit = new boolean[10000];
+
+			bfs(value, answer);
 		}
-		bfs();
 
+		System.out.println(sb);
 	}
 
-	public static void bfs() {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(1);
-		cnt[1] = 0;
-		visit[1] = true;
+	public static void bfs(int value, int answer) {
+		Queue<Number> queue = new LinkedList<>();
+		queue.add(new Number(value, ""));
 
+		visit[value] = true;
 		while (!queue.isEmpty()) {
-			int nowLocation = queue.poll();
-			if (nowLocation == 100) {
-				System.out.println(cnt[100]);
+			int num = queue.peek().num;
+			String word = queue.peek().word;
+
+			queue.poll();
+
+			if (num == answer) {
+				sb.append(word).append("\n");
 				return;
 			}
 
-			for (int i = 1; i < 7; i++) {
-				int nextLocation = nowLocation + i;
-				if (nextLocation < 101 && !visit[nextLocation] ) {
-					visit[nextLocation] = true;
+			int D = (num * 2) % 10000;
 
-					if (ladderAndSnake[nextLocation] > 0) {
-						if (!visit[ladderAndSnake[nextLocation]]) {
-							cnt[ladderAndSnake[nextLocation]] = cnt[nowLocation] + 1;
-							visit[ladderAndSnake[nextLocation]] = true;
-							queue.offer(ladderAndSnake[nextLocation]);
-						}
-					} else {
-						cnt[nextLocation] = cnt[nowLocation] + 1;
-						queue.offer(nextLocation);
-					}
-				}
+			int S = num - 1;
+
+			if (S == -1) {
+				S = 9999;
 			}
+
+			int L = (num % 1000) * 10 + num / 1000;
+
+			int R = num / 10 + (num % 10) * 1000;
+
+			if (!visit[D]) {
+				visit[D] = true;
+				queue.offer(new Number(D, word + "D"));
+			}
+			if (!visit[S]) {
+				visit[S] = true;
+				queue.offer(new Number(S, word + "S"));
+			}
+			if (!visit[L]) {
+				visit[L] = true;
+				queue.offer(new Number(L, word + "L"));
+			}
+			if (!visit[R]) {
+				visit[R] = true;
+				queue.offer(new Number(R, word + "R"));
+			}
+		}
+	}
+
+	public static class Number{
+		int num;
+		String word;
+
+		public Number(int num, String word) {
+			this.num = num;
+			this.word = word;
 		}
 	}
 }
