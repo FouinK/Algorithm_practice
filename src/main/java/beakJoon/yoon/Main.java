a1a6a023b627ef3a2c32d9c9cc221975ad78403a
Fouink <yyss9103@gmail.com>
2023-01-03 22:51:08 +0900

    프로그래머스 level 2 마법의 엘리베이터

--- src/main/java/beakJoon/yoon/Main.java
+++ src/main/java/beakJoon/yoon/Main.java
@@ -1,70 +1,36 @@
 package beakJoon.yoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.StringTokenizer;
-
 public class Main {
+	static int storey = 6665777;
+	public static void main(String[] args)   {
+		int tempStorey = storey;
+		int cnt = 0;
+		while(tempStorey>0){
+			int ext = tempStorey%10;
+			tempStorey/=10;
+			int value = 0;
 
-	static int[] arr;
-	static boolean[] visit;
-	static int[] answer;
-	static int N;
-	static int M;
-	static LinkedHashSet<String> ans;
+			if(ext==5){
+				if(tempStorey%10 >=5) {
+					tempStorey++;
+					value = 10-ext;
+				}
+				else {
+					value = ext;
+				}
+			}else if(ext > 5) {
+				tempStorey++;
+				value = 10 - ext;
+			}
+			else {
+				value = ext;
+			}
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+			cnt += value;
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		arr = new int[N];
-		visit = new boolean[N];
-		answer = new int[M];
-		ans = new LinkedHashSet<>();
-
-		st = new StringTokenizer(br.readLine());
-
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-
-		Arrays.sort(arr);
-
-		dfs(0);
-
-		StringBuilder sb = new StringBuilder();
-
-		for (String an : ans) {
-			sb.append(an).append("\n");
-		}
-
-		System.out.println(sb);
+		System.out.println(cnt);
 	}
 
-	public static void dfs(int depth) {
-		if (depth == M) {
-			StringBuilder sb = new StringBuilder();
-			for (int value : answer) {
-				sb.append(value).append(" ");
-			}
-			ans.add(sb.toString());
-			return;
-		}
-
-		for (int i = 0; i < arr.length; i++) {
-			if (!visit[i]) {
-				answer[depth] = arr[i];
-				visit[i] = true;
-				dfs(depth + 1);
-				visit[i] = false;
-			}
-		}
-	}
 }
